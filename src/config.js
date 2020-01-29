d487a209cc870cad56cf7f33001c7722cd188c1d
Lawson <islawson@f0189856da26.ant.amazon.com>
2020-01-29 10:08:29 -0500

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "canoja-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://8lanr3jgd2.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_leo7oX5e9",
+    APP_CLIENT_ID: "7cnvjil9oen6j98b7is3i8pv2t",
+    IDENTITY_POOL_ID: "us-east-1:8f4e10a8-10c2-40d9-8fd4-b2d27bf25dba"
+  }
+};
